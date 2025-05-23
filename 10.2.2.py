14e647511eb2e0caf7be948cffe78b28420f4378
opilane <opilane@HP-CZC2349HTC>
2025-05-23 15:31:29 +0300

    ööm

--- 10.2.2.py
+++ 10.2.2.py
@@ -9,17 +9,18 @@ pind=pygame.display.set_mode([ekraan_k,ekraan_l])
 pygame.display.set_caption("Harjutamine")
 pind.fill(lfon)
 
-def joonista_ruudustik(pind,ruudu_l,ruudu_k, read, veerud):
-    for rida in range(read):
+def joonista_ruudustik(pind,ruudu_l,ruudu_k, rida, veerud):
+    for rida in range(rida):
         for veerg in range(veerud):
             värv=[random.randint(0,255),random.randint(0,255),random.randint(0,255)]
             x=veerg*ruudu_l
             y=rida*ruudu_k
-            pygame.draw.rect(pind,värv,(x,y,ruudu_l, ruudu_k), 1)
+            pygame.draw.rect(pind,värv,(x,y,ruudu_l, ruudu_k),1)
+            
 ruudu_k=random.randint(10,50)
 ruudu_l=random.randint(10,50)
 read=ekraan_l//ruudu_l+1
-veerud=ekraan_l//ruudu_k+1
+veerud=ekraan_k//ruudu_k+1
 
 joonista_ruudustik(pind,ruudu_k,ruudu_l,read,veerud)
 
